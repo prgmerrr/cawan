6d26455065685f3a471d3fe2601cd89389d6ca06
ibnu-zaky <145089602+ibnu-zaky@users.noreply.github.com>
2023-09-16 21:07:26 +0700

    Add files via upload

---
+++ Project 1 Coffe Shop Cawan/js/script.js
@@ -0,0 +1,14 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// when hamburger menu clicked
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
